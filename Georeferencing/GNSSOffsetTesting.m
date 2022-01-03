c0899b221efb5c8468578aa2877e4d5a6f63ae85
tjkayser <tjkayser@web.de>
2022-01-03 14:58:16 +0100

    Updated Rotation for simulateGNSS.m

--- Georeferencing/GNSSOffsetTesting.m
+++ Georeferencing/GNSSOffsetTesting.m
@@ -1,4 +1,4 @@
-%% Main Code for simulateGNSS.m Testing
+    %% Main Code for simulateGNSS.m Testing
 
 
 clear
@@ -11,9 +11,11 @@ data_IP = table2array(readtable(".\Data\ScanRTKSecondRun.txt"));
 
 %% Call simulateGNSS
 
-additionalRot = [1 0 0; 0 1 0; 0 0 1];
+additionalRot = [0 0 1; -1 0 0; 0 -1 0];
+dir = 2;
 
-data_GNSS_Sim = simulateGNSS(data_IP,[-0.011 0.134 0.202], additionalRot);
+
+data_GNSS_Sim = simulateGNSS(data_IP,[-0.011 0.134 0.202], additionalRot, dir);
 
 %% Rotation matrix from scanner quaternions
 R =quat2rotm(data_IP(:,5:8));
@@ -27,6 +29,11 @@ R =quat2rotm(data_IP(:,5:8));
 
 
 figure(1)
+h1 = axes;
+set (h1, 'Xdir', 'reverse');
+set (h1, 'Ydir', 'reverse');
+set(h1, 'XAxisLocation', 'Top')
+set(h1, 'YAxisLocation', 'Right')
 hold on
 axis equal
 
@@ -66,40 +73,50 @@ for i=1800:10:8000 %size(data_GNSS_Sim,1)
     % Set car part and axis coordinates
     xQuat = [1 0; 0 0; 0 0];
     yQuat = [0 0; 1 0; 0 0];
-    cart=[.8 -.8 -.8 .8; .5 .5 -.5 -.5];
-    wheel1= [1.1 .5 .5 1.1; .4 .4 .75 .75];
-    wheel2= [-1.1 -.5 -.5 -1.1; -.4 -.4 -.75 -.75];
-    wheel3= [1.1 .5 .5 1.1; -.4 -.4 -.75 -.75];
-    wheel4= [-1.1 -.5 -.5 -1.1; .4 .4 .75 .75];
+    cart=[.5 .5 -.5 -.5; .8 -.8 -.8 .8];
+    wheel1= [ .4 .4 .75 .75; 1.1 .5 .5 1.1];
+    wheel2= [-.4 -.4 -.75 -.75; -1.1 -.5 -.5 -1.1];
+    wheel3= [-.4 -.4 -.75 -.75; 1.1 .5 .5 1.1];
+    wheel4= [.4 .4 .75 .75; -1.1 -.5 -.5 -1.1];
     
-    cart= R(1:2,1:2,i)*additionalRot(1:2,1:2) *cart;
+    switch dir
+        case 1
+            RotMat = additionalRot * R(:,:,i);
+            
+        case 2
+            RotMat = R(:,:,i) * additionalRot;
+            
+    end
+   
+
+    cart= RotMat(1:2,1:2) * cart;
     cart=cart';
     cart=cart+data_IP(i,2:3);
-    
-    wheel1= R(1:2,1:2,i)*additionalRot(1:2,1:2) *wheel1;
+
+    wheel1= RotMat(1:2,1:2) * wheel1;
     wheel1=wheel1';
     wheel1=wheel1+data_IP(i,2:3);
-    
-    wheel2= R(1:2,1:2,i)*additionalRot(1:2,1:2) *wheel2;
+
+    wheel2= RotMat(1:2,1:2) * wheel2;
     wheel2=wheel2';
     wheel2=wheel2+data_IP(i,2:3);
-    
-    wheel3= R(1:2,1:2,i)*additionalRot(1:2,1:2) *wheel3;
+
+    wheel3= RotMat(1:2,1:2) * wheel3;
     wheel3=wheel3';
     wheel3=wheel3+data_IP(i,2:3);
-    
-    wheel4= R(1:2,1:2,i)*additionalRot(1:2,1:2) *wheel4;
+
+    wheel4= RotMat(1:2,1:2) * wheel4;
     wheel4=wheel4';
     wheel4=wheel4+data_IP(i,2:3);
-    
-    xQuat=R(:,:,i) * additionalRot * xQuat;
+
+    xQuat= RotMat * xQuat;
     xQuat=xQuat(1:2,1:2)';
     xQuat=xQuat+data_IP(i,2:3);
-    
-    yQuat=R(:,:,i) * additionalRot *yQuat;
+
+    yQuat= RotMat * yQuat;
     yQuat=yQuat(1:2,1:2)';
     yQuat=yQuat+data_IP(i,2:3);
-    
+            
     % Plot trajectories
     plot(data_IP(1800:i,2),data_IP(1800:i,3),'r-.')
     plot(data_GNSS_Sim(i,2), data_GNSS_Sim(i,3),'k.')
--- Georeferencing/simulateGNSS.m
+++ Georeferencing/simulateGNSS.m
@@ -1,15 +1,16 @@
 %% Simulate GNSS Trajectory in Scanner System with Lever Arm Offset and Scanner Orientation
 % ----------------------------------------------------------------------------
-% Input:       traj_scan        -   Scanner Trajectory with Orientation (variable type table or matrix)
-%              leverArm         -   Lever Arm between Scanner and GNSS Antenna in Scanner Coordinate System (1x3 or 3x1 matrix) 
-%              additionalRot    -   3x3 Rotation Matrix  for additional Quaternion Rotation
-% Output:      traj_GNSS        -   GNSS Antenna Trajectory in Scanner Coordinate System (output variable typ matching traj_input type)
+% Input:        traj_scan        -   Scanner Trajectory with Orientation (variable type table or matrix)
+%               leverArm         -   Lever Arm between Scanner and GNSS Antenna in Scanner Coordinate System (1x3 or 3x1 matrix) 
+%               additionalRot    -   3x3 Rotation Matrix  for additional Quaternion Rotation
+%               dir              -   just for testing
+% Output:       traj_GNSS        -   GNSS Antenna Trajectory in Scanner Coordinate System (output variable typ matching traj_input type)
 % ----------------------------------------------------------------------------
 % Authors:     SIMP-Project Team
 % ----------------------------------------------------------------------------
 % Last Modified:   05.12.2021
 
-function [traj_GNSS] = simulateGNSS(traj_scan, leverArm, additionalRot)
+function [traj_GNSS] = simulateGNSS(traj_scan, leverArm, additionalRot, dir)
 
 
 % Check data type to match output to input trajectory
@@ -34,16 +35,29 @@ else
     error("Invalid lever arm input. Please give a 1x3 or 3x1 numeric array");
 end
 
+
+
+
 % Rotate lever arm and add to scanner position
 for i=1:size(traj_scan,1)
     switch dat_type
         case 'mat'
             rotMat = quat2rotm(traj_scan(i,5:8));
-            leverArm_i = rotMat * additionalRot * leverArm;
+            switch dir
+                case 1
+                    leverArm_i = additionalRot*rotMat* leverArm;
+                case 2
+                    leverArm_i = rotMat*additionalRot* leverArm;
+            end
             traj_GNSS(i,2:4) = traj_scan(i,2:4) + leverArm_i';
         case 'table'
             rotMat = quat2rotm(traj_scan{i,5:8});
-            leverArm_i = rotMat * additionalRot * leverArm;
+            switch dir
+                case 1
+                    leverArm_i = additionalRot*rotMat* leverArm;
+                case 2
+                    leverArm_i = rotMat*additionalRot* leverArm;
+            end
             traj_GNSS{i,2:4} = traj_scan{i,2:4} + leverArm_i';
     end
 end
