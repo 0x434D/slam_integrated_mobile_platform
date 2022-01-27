e6827aee20d7c1464eb2d4d3c03a502894a328eb
Johannes Ernst <johannes.ernst98@gmail.com>
2022-01-27 12:16:40 +0100

    Added colorCoding as function
    - Cleared up Georeferencing
    - Added comments to Georeferencing to see progress when code is running
    - Included colorCoding as function in Georeferencing
    - Deleted some unneccessary files
    - Moved colorCoding and tra2ori into Functions folder

--- Georeferencing/Code/Georeferencing.m
+++ Georeferencing/Code/Georeferencing.m
@@ -1,14 +1,16 @@
-%% Georeferencing - Trajectory matching
+%% Georeferencing
 % Author: SIMP Team
 clearvars
 close all
 format longG
 clc
-fprintf('Georeferencing - Trajectory matching\n')
+fprintf('Georeferencing:\n')
 
 addpath(genpath('..\'))
 
-%% Load Data and define parameters
+%% Load data and define parameters
+fprintf('\nLoading Data\n')
+
 % Define lever arm [m]
 leverArm = [-0.011 0.134 0.397]';       % from CAD model
 step = 0.1;
@@ -16,52 +18,68 @@ step = 0.1;
 [GNSSFileName,GNSSPathName,~] = uigetfile('*.txt;*.mat', 'Select the input GNSS Trajectory File', '..\Data');
 [ScanTFileName,ScanTPathName,~] = uigetfile('*.txt;*.mat', 'Select the input Scan Trajectory File', '..\Data');
 [ScanPCFileName,ScanPCPathName,~] = uigetfile('*.laz;*.las', 'Select the input Scan Pointcloud File', '..\Data');
+imgPath = uigetdir('..\Data','Select GoPro Image Folder');
 
 % Load GNSS trajectory (format: [time [s], X [m], Y [m], Z [m]])
+fprintf('\tLoading GNSS trajectory\n')
 load([GNSSPathName GNSSFileName],'POS','POS_label','GPS_1','GPS_label');
-% load('Data\testGNSS_RTK.mat','POS','POS_label','GPS2','GPS2_label');
 
-% Select GPS2 (GNSS only) or KF-Solution (GNSS + IMU) and transform to flat
+% Select GPS1 (GNSS only) or KF-Solution (GNSS + IMU) and transform to flat
 % earth frame for easier usage and orientation
-% GNSS = lla2ecef(POS(:,3:5));            % transform to ECEF [m]
-% GNSS = [POS(:,2)*1e-6 GNSS];            % add time in [s]
-% GNSS = lla2ecef(GPS2(:,8:10));          % transform to ECEF [m]
-% GNSS = [GPS2(:,2)*1e-6 GNSS];           % add time in [s]
 flatRef = [mean(GPS_1(:,9:10)) 0];                  % reference point for flat earth [°, °, m]
 GNSS = lla2flat(GPS_1(:,9:11),flatRef(1:2),0,0);    % flat earth coordinates [m]
 GNSS = [GNSS(:,2) GNSS(:,1) GNSS(:,3)];             % Switch order to X, Y, Z
 
 % Load Scanner trajectory (format: [time [s], X [m], Y [m], Z [m] ...])
+fprintf('\tLoading Scan trajectory\n')
 ScanRaw = load([ScanTPathName ScanTFileName]);
-% ScanRaw = load('Data\testSCAN_RTK.txt');
 
 % Add lever arm to scan trajectory with orientation from scanner
 Scan = simulateGNSS(ScanRaw, leverArm);
 % Scan = ScanRaw;
-ScanBackup = Scan;                        % save original Scan trajectory
+ScanBackup = Scan;                    	% save original Scan trajectory
 
 % Reduce scan trajectory times 
 Scan(:,1) = Scan(:,1)-Scan(1,1);
 
 % Load Scan Point Cloud Data
+fprintf('\tLoading point cloud\n')
 ScanPC = lasdata([ScanPCPathName, ScanPCFileName], 'loadall');
 
+% Load image data
+fprintf('\tLoad image data\n')
+addpath(imgPath)
+imgList = ls(imgPath);
+imgList = imgList(3:end,:); % first two entries are not files
+
+% Read images (and dimensions, times) in a loop and save in img structure
+dsFactor = 3;                                           % downsample factor
+iDim = [2880/dsFactor 5760/dsFactor 3 size(imgList,1)];	% size of img array
+img = zeros(iDim(1),iDim(2),iDim(3),iDim(4),'uint8');  	% [x y rgb frame]
+wait = waitbar(0,"Loading images (0/" + num2str(size(imgList,1)) + ")");
+for i = 1:size(imgList,1)
+    fullImg = imread(imgList(i,:));
+    img(:,:,:,i) = fullImg(1:dsFactor:end,1:dsFactor:end,:);
+    frameDates(i,:) = datetime(imfinfo(imgList(i,:)).DateTime,...
+                               'InputFormat','yyyy:MM:dd HH:mm:ss');
+    frameTimes(i,1) = datenum(frameDates(i,:))*24*3600;	% MATLAB time [s]
+    waitbar(i/size(imgList,1),wait,"Loading images (" + num2str(i) + "/"+ num2str(size(imgList,1)) + ")");
+end
+close(wait);
+
 %% Calculate Time Offset
+fprintf('\nCalculating time offset\n')
+
 % Get GPS time 
 epoch = datetime(1980,1,6,'TimeZone','UTCLeapSeconds');
 dtUTC = datenum(epoch + days(GPS_1(:,6)*7) + seconds(GPS_1(:,5)*1e-3) + hours(1));
-% datetime(dtUTC,'ConvertFrom','datenum','Format', 'yyyy-MM-dd HH:mm:ss.SSS')
 GNSS = [dtUTC*24*3600 GNSS];    % add time in [seconds]
-% GNSS = [GPS2(:,2)*1e-6 GNSS]; 
 
-% Delete GNSS measurements bevore moving via time matching
+% Delete GNSS measurements before moving via time matching
 [timeOffset,~] = findTimeDelay(Scan, [GNSS(:,2:4) GNSS(:,1)], 0.01);
-close all   
+close all
 [~,idx] = min(abs(GNSS(:,1)-GNSS(1,1)-timeOffset)); % find index of time delay
 GNSS = GNSS(idx:end,:);                             % now same trajectory start as Scan
-
-% Save GNSS trajectory as variable
-% save('GNSS_Trajectory_SchlossRun.mat','GNSS');
 
 % Get mean time step size
 ScanTSS = mean(diff(Scan(:,1)));
@@ -71,12 +89,8 @@ GNSSTSS = mean(diff(GNSS(:,1)));
 timeOffset = GNSS(1,1)-Scan(1,1);
 Scan(:,1) = Scan(:,1)+timeOffset;
 
-% % Reduce coordinates for numeric stability (relevant for ECEF)
-% offset = mean(GNSS(:,2:4));
-% GNSS(:,2:4) = GNSS(:,2:4)-offset;
-% % Scan(:,2:4) = Scan(:,2:4)-offset;
-
 %% Coarse trajectory match
+fprintf('\nCoarse trajectory match\n')
 [Scan,rotScale,translation] = coarseMatch(GNSS, Scan, timeOffset);
 
 % Plot trajectories
@@ -89,16 +103,14 @@ plot3(Scan(:,2),Scan(:,3),Scan(:,4),'g')
 legend('GNSS','SCAN','Location','NorthWest')
 title('Coarse Trajectory Match Results')
 view([90 90])
-% print('-dpng','-r200',"CoarseTrafo.png")
 
 %% Accurate trajectory match
+fprintf('\nAccurate trajectory match\n')
 [Scan,rotScale,translation] = accurateMatch(GNSS, Scan, timeOffset, rotScale, translation, 7, 200, 1, 0);
 
-% % Add initial offset (relevant for ECEF)
-% translation = translation + offset';
-% GNSS(:,2:4) = GNSS(:,2:4) + offset;
-
 %% Test transformation on original trajectory
+fprintf('\nTest transformation on original trajectory\n')
+
 % Transform trajectory and plot anew
 figure
 plot3(GNSS(:,2),GNSS(:,3),GNSS(:,4),'b')
@@ -109,16 +121,14 @@ for i = 1:length(ScanBackup)
    ScanBackup(i,2:4) = rotScale * ScanBackup(i,2:4)' + translation;
 end
 plot3(ScanBackup(:,2),ScanBackup(:,3),ScanBackup(:,4),'g')
-% axis equal
+axis equal
 legend('GNSS','SCAN','Location','NorthWest')
 title('Combined Transformation (Coarse + Accurate)')
 view([90 90])
-% print('-dpng','-r200',"AccurateTrafo_Good.png")
-
-% Save Scan trajectory as variable
-% save('Scan_Trajectory_SchlossRun.mat','Scan','rotScale');
 
 %% Estimate point matching accuracy
+fprintf('\nEstimate point matching accuracy\n')
+
 % Find corresponding match points in a loop
 bound = 200;
 for i = 1:length(GNSS)
@@ -185,25 +195,19 @@ ylabel('Distance [m]')
 % print('-dpng','-r200',"Error_withoutLever.png")
 
 %% Transform Point Cloud
+fprintf('\nTransform point cloud\n')
 PC_transf = [ScanPC.x, ScanPC.y, ScanPC.z] * rotScale' + translation';
 
-% Save for ColorCoding in Flat Earth (remove when including ColorCoding)
-ScanPC.header.max_x = max(PC_transf(:,1));
-ScanPC.header.min_x = min(PC_transf(:,1));
-ScanPC.header.max_y = max(PC_transf(:,2));
-ScanPC.header.min_y = min(PC_transf(:,2));
-ScanPC.header.max_z = max(-PC_transf(:,3));
-ScanPC.header.min_z = min(-PC_transf(:,3));
+%% Colorize Point Cloud
+fprintf('\nColorize point cloud\n')
+RGB = colorCoding(PC_transf, ScanPC.gps_time, ScanBackup(1,1),...
+                  timeOffset, GNSS, Scan, rotScale, iDim, img, frameTimes);
+ScanPC.red = uint16(RGB(:,1));
+ScanPC.green = uint16(RGB(:,2));
+ScanPC.blue = uint16(RGB(:,3));
 
-ScanPC.header.x_offset = mean(PC_transf(:,1));
-ScanPC.header.y_offset = mean(PC_transf(:,2));
-ScanPC.header.z_offset = mean(-PC_transf(:,3));
-
-ScanPC.y = PC_transf(:,1);%-ScanPC.header.x_offset;
-ScanPC.x = PC_transf(:,2);%-ScanPC.header.y_offset;
-ScanPC.z = -PC_transf(:,3);%-ScanPC.header.z_offset;
-
-% write_las(ScanPC, [ScanPCPathName, 'GeoreferencedPointcloud3Schloss.las']);
+%% Save final cloud
+fprintf('\nSave final cloud\n')
 
 % Save finally in ECEF
 PC_transf = lla2ecef(flat2lla(PC_transf, flatRef(1:2),0, 0));
@@ -219,12 +223,11 @@ ScanPC.header.x_offset = mean(PC_transf(:,1));
 ScanPC.header.y_offset = mean(PC_transf(:,2));
 ScanPC.header.z_offset = mean(PC_transf(:,3));
 
-ScanPC.x = PC_transf(:,1);%-ScanPC.header.x_offset;
-ScanPC.y = PC_transf(:,2);%-ScanPC.header.y_offset;
-ScanPC.z = PC_transf(:,3);%-ScanPC.header.z_offset;
+ScanPC.x = PC_transf(:,1);
+ScanPC.y = PC_transf(:,2);
+ScanPC.z = PC_transf(:,3);
 
-% write_las(ScanPC, [ScanPCPathName, 'GeoreferencedPointcloud.las']);
+write_las(ScanPC, [ScanPCPathName, 'GeoreferencedPointcloud.las']);
 
 %% TODO: 
-%       - Scan mehr Punkte oder GNSS meh Punkte unabhängig
 %       - Further improve weighting based on GNSS reliability
---
+++ Georeferencing/Code/functions/colorCoding.m
@@ -0,0 +1,120 @@
+function pointColors = colorCoding(pointCloud,pointTimes,firstTime,timeOffset,gnss,scan,rotScale,iDim,img,frameTimes)
+% Uses captured 3D-GoPro images of a run to colorize the point cloud that 
+% was previously georeferenced.
+% -------------------------------------------------------------------------
+% Autor: SIMP-Project Team
+% -------------------------------------------------------------------------
+% To connect the images and the point cloud, we use the gps timestamps in a
+% combined time frame. As a first step we calculate all corresponding
+% points for one image based on the frame time and safe the corresponding
+% indices. Thus, each point is only colored once by one specific image.
+% Next, we prepare the image by first calculating the orientation (azimuth)
+% from the scan quaternions. We also derive the position of the GoPro from
+% the GNSS antenna position and a small z-offset (GoPro is below antenna).
+% Finally we calculate spheric coordinates of each point with respect to
+% the camera position in a flat-earth frame and from that derive two angles
+% to get a pixel position and corresponding RGB.
+% -------------------------------------------------------------------------
+% Input:    See variable names from Georeferencing
+% Output:   pointColors = RGB colors for all points (uint8)
+% -------------------------------------------------------------------------
+
+%% Connect image and scan data via timestamps
+fprintf('\tConnect image and scan data via timestamps\n')
+
+% Transform point times into MATLAB datetime [seconds]
+pointTimes = pointTimes - firstTime + timeOffset;
+
+% Loop over all images to find the closest scan-point index for each frame
+for i = 1:length(frameTimes)
+    [~,idx] = min(abs(pointTimes-frameTimes(i)));
+    pointIdx(i,1) = idx;
+end
+
+% Define the scan points for each image with indices of bounds
+% First image: [bounds(1) bounds(2)], Second: [bounds(2) bounds(3)] ...
+idxBounds = [1;pointIdx(1:end-1)+floor(diff(pointIdx)/2);length(pointTimes)];
+
+%% Prepare spatial data
+fprintf('\tPrepare spatial data\n')
+
+% Switch x- and y-axis and flip z-axis of all spatial data because
+% flat-earth convention is not suitable for colorCoding
+gnss = [gnss(:,1) gnss(:,3) gnss(:,2) -gnss(:,4)];
+scan = [scan(:,1) scan(:,3) scan(:,2) -scan(:,4) scan(:,5:8)];
+pointCloud = [pointCloud(:,2) pointCloud(:,1) -pointCloud(:,3)];
+
+%% Loop over all images
+fprintf('\tLoop over all images\n')
+pointColors = uint8(zeros(size(pointTimes,1),3));
+for i = 1:size(frameTimes,1)-1
+%% Get orientation and position of current frame
+% Add experimental time offset to frame times because the actual recording 
+% time is always a little earlier
+time = frameTimes(i)-0.7;
+
+% Function to get current orientation from GNSS trajectory 
+[orient,camOrig] = tra2ori(gnss,scan,time,rotScale);
+
+% Load corresponding scan points for image
+points = pointCloud(idxBounds(i):idxBounds(i+1),:);
+
+%% Find RGB for each scan point
+% Define origin and calculate the vector of each scan point to origin
+camOrig = camOrig - [0 0 0.13];	% camera position (13 cm below antenna)
+diffVec = points-camOrig;
+
+% Spheric coordinates (two angles) from cartestian coordinates
+[th,phi,~] = cart2sph(diffVec(:,1),diffVec(:,2),diffVec(:,3));
+
+% Adjust orientation with vehicle trajectory 
+th = th+pi-(3*pi/2-orient);     % this if scanner is at the sides
+% th = th+pi-(pi/2-orient);     % this if scanner is in the middle
+
+% Spheric angles to MATLAB plot pixel positions
+% (-1 and +1 to assure result to be within [1 1920] or [1 1080])
+xImg = round(mod(th,2*pi)*(iDim(2)-1)/(2*pi))+1;
+yImg = round((pi/2-phi)*(iDim(1)-1)/pi)+1;
+
+% Extract RGB color of the pixel(s) from 4D picture matrix
+one = ones(length(xImg),1);
+rgb = [img(sub2ind(size(img), yImg, xImg, one*1, one*i))...
+       img(sub2ind(size(img), yImg, xImg, one*2, one*i))...
+       img(sub2ind(size(img), yImg, xImg, one*3, one*i))];
+rgb = uint8(rgb);              % convert to uint8 for .las color
+
+% Add up detected point colors for all images
+pointColors(idxBounds(i):idxBounds(i+1),:) = rgb;
+
+%% Showcase
+% Show rotated image (pick with i) and current scan points to be colored
+if i == 31
+    figure 
+    imshow(img(:,:,:,i))
+    hold on
+    scatter(xImg,yImg,7,'b','filled')
+    drawnow
+    
+    figure
+    pcshow(pointCloud(idxBounds(i):idxBounds(i+1),:),rgb)
+end
+end
+
+%% Plot whole point cloud
+f1 = figure;
+set(f1, 'Position', [400 50 1400 800])
+view([0 25])
+pcshow(pointCloud,pointColors);
+
+% gif('SchlossV01.gif','DelayTime',1/40,'overwrite',true)
+% for i = 0:360
+%    view([i 25])
+%    gif
+% end
+
+%% TODO:
+% - Scan coordinates should be in flat earth frame
+% - Create a mask for Scanner and antenna platform
+% - Test with video instead of images?
+end
+
