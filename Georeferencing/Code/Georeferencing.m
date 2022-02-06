51cca7370c61c9bcb8889208501a741c25ed4dc8
tjkayser <tjkayser@web.de>
2022-02-06 19:38:08 +0100

    - Added simpModel.glb, a binary 3D model file of the SIMP platform - Added writeCZML.m Function to write a .czml in the output folder which combines the gnss trajectory with the simpModel.glb to create an animation in cesium - Added KMLwrite line in Georeferencing.m for possible .kml output of the gnss trajectory

--- Georeferencing/Code/Georeferencing.m
+++ Georeferencing/Code/Georeferencing.m
@@ -34,7 +34,7 @@ GPS_1EH = [GPS_1(:,9:10), GPS_1(:,11)+geoidHeight];
 % earth frame for easier usage and orientation
 flatRef = [mean(GPS_1EH) 0];                  % reference point for flat earth [°, °, m]
 GNSS = lla2flat(GPS_1EH,flatRef(1:2),0,0);    % flat earth coordinates [m]
-GNSS = [GNSS(:,2) GNSS(:,1) GNSS(:,3)];             % Switch order to X, Y, Z
+GNSS = [GNSS(:,2) GNSS(:,1) GNSS(:,3)];       % Switch order to X, Y, Z
 
 % Load Scanner trajectory (format: [time [s], X [m], Y [m], Z [m] ...])
 fprintf('\tLoading Scan trajectory\n')
@@ -281,8 +281,13 @@ write_las(ScanPC, [outputPath, 'Pointcloud.las']);
 % Zip Las File to Laz
 system(['.\functions\laszip.exe -i ' outputPath 'Pointcloud.las -o ' outputPath 'Pointcloud.laz']);
 
-% Save GNSS trajectory as KML
+% Save GNSS trajectory as CZML or KML
+% CZML output for path with animation
+czmlFileName = 'pathTour.czml';
 
-filename = 'trajectory.kml';
+writeCZML(dtUTC(200:5:end)*24*3600,GPS_1EH(200:5:end,1),GPS_1EH(200:5:end,2),GPS_1EH(200:5:end,3), [outputPath, czmlFileName]);
 
-kmlwriteline([outputPath, filename], GPS_1EH(:,1), GPS_1EH(:,2), GPS_1EH(:,3), 'Description', 'Trajectory Created by Georeferencing.m', 'Name', 'Trajectory', 'Color', 'red', 'LineWidth', 3);
+% KML output for path without animation
+% kmlFileName = 'trajectory.kml';
+% 
+% kmlwriteline([outputPath, kmlFileName], GPS_1EH(:,1), GPS_1EH(:,2), GPS_1EH(:,3), 'Description', 'Trajectory Created by Georeferencing.m', 'Name', 'Trajectory', 'Color', 'red', 'LineWidth', 3);
